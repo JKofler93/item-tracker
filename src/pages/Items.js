ba87c57041bbe94cc4d3a72356f581215f06af13
JKofler93 <Koflerj93@gmail.com>
2021-10-01 17:04:43 -0400

    started making components

--- src/pages/Items.js
+++ src/pages/Items.js
@@ -1,52 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import { withStyles, makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
 import Info from '../components/Info';
+import Inputs from '../components/Inputs';
+import Buttons from '../components/Buttons';
+import ItemTable from '../components/ItemTable'
 
-import { useSelector, useDispatch } from "react-redux"
+import { useDispatch } from "react-redux"
 import { loadItems, assignItem, unassignItem, completeItem } from '../redux/actions';
-
-const StyledTableCell = withStyles((theme) => ({
-  head: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  body: {
-    fontSize: 14,
-  },
-}))(TableCell);
-
-const StyledTableRow = withStyles((theme) => ({
-  root: {
-    '&:nth-of-type(odd)': {
-      backgroundColor: theme.palette.action.hover,
-    },
-  },
-}))(TableRow);
-
-
-const useStyles = makeStyles({
-  table: {
-    marginTop: 100,
-    minWidth: 900,
-  },
-});
 
 function Items() {
   const [filter, setFilter] = useState("");
 
-
-
   let dispatch = useDispatch()
-  const { items } = useSelector(state => state.data)
-
-  const classes = useStyles();
 
   useEffect(() => {
     dispatch(loadItems())
@@ -112,48 +76,12 @@ function Items() {
   return (
     <>
       <div className="reset">
-        {filter === "all" ? <button className="reset-button" style={{"backgroundColor": "blue", "marginRight": "20px"}} onClick={() => setFilter("all")}>All</button> : <button className="reset-button" style={{"marginRight": "20px"}} onClick={() => setFilter("all")}>All</button>}
-        {filter === "assigned" ? <button className="reset-button" style={{"backgroundColor": "blue", "marginRight": "20px"}} onClick={() => setFilter("assigned")}>Assigned</button> : <button className="reset-button" style={{"marginRight": "20px"}} onClick={() => setFilter("assigned")}>Assigned</button>}
-        {filter === "unassigned" ? <button className="reset-button" style={{"backgroundColor": "blue", "marginRight": "20px"}} onClick={() => setFilter("unassigned")}>Unassigned</button> : <button className="reset-button" style={{"marginRight": "20px"}} onClick={() => setFilter("unassigned")}>Unassigned</button>}
-        {filter === "completed" ? <button className="reset-button" style={{"backgroundColor": "blue", "marginRight": "20px"}} onClick={() => setFilter("completed")}>Completed</button> : <button className="reset-button" style={{"marginRight": "20px"}} onClick={() => setFilter("completed")}>Completed</button>}
+          <Buttons filter={filter} setFilter={setFilter}/>
           <Info/>
-          <input className="provider-search-bar" style={{"marginTop": "20px"}} placeHolder="Search by Provider" onChange={(e) => setFilter(e.target.value)}></input>
-          <input className="provider-search-bar" style={{"marginTop": "20px"}} placeHolder="Search by Serial Number" onChange={(e) => setFilter(e.target.value)}></input>
+          <Inputs setFilter={setFilter}/>
       </div>
-
       <div>
-        <TableContainer component={Paper}>
-          <Table className={classes.table} aria-label="customized table">
-              <TableHead>
-                      <TableRow>
-                          <StyledTableCell id="table-head" align="center">ID</StyledTableCell>
-                          <StyledTableCell id="table-head" align="center">Provider</StyledTableCell>
-                          <StyledTableCell id="table-head" align="center">Model</StyledTableCell>
-                          <StyledTableCell id="table-head" align="center">Serial Number</StyledTableCell>
-                          <StyledTableCell id="table-head" align="center">Status</StyledTableCell>
-                          {filter === "assigned" || filter === "all" ? <StyledTableCell id="table-head" align="center">Assigned At</StyledTableCell> : null}
-                          {filter === "completed" || filter === "all" ? <StyledTableCell id="table-head" align="center">Completed At</StyledTableCell> : null}
-                          {filter === "unassigned" || filter === "all" ? <StyledTableCell id="table-head" align="center">Created At</StyledTableCell> : null}
-                          <StyledTableCell id="table-head" align="center">Actions</StyledTableCell>
-                      </TableRow>
-                    </TableHead>
-                    <TableBody>
-                    {filterItems(items).map((item) =>  ( 
-                        <StyledTableRow key={item.id}>
-                          <StyledTableCell id="item-info" align="center">{item.id}</StyledTableCell>
-                          <StyledTableCell id="item-info" align="center">{item.provider}</StyledTableCell>
-                          <StyledTableCell id="item-info" align="center">{item.model}</StyledTableCell>
-                          <StyledTableCell id="item-info" align="center">{item.serial}</StyledTableCell>
-                          <StyledTableCell id={item.status === "assigned" || item.status === "unassigned" ? "active" : "completed"} align="center">{item.status}</StyledTableCell>
-                          {filter === "assigned" || filter === "all" ?<StyledTableCell id="item-info" align="center">{item.assigned_at}</StyledTableCell> : null}
-                          {filter === "completed" || filter === "all"? <StyledTableCell id="item-info" align="center">{item.completed_at}</StyledTableCell> : null}
-                          {filter === "unassigned" || filter === "all" ? <StyledTableCell id="item-info" align="center">{item.created_at}</StyledTableCell> : null}
-                          <StyledTableCell align="center">{decideButtons(item.id, item.status)}</StyledTableCell>
-                        </StyledTableRow>
-                    ))}
-                    </TableBody>
-          </Table>
-        </TableContainer>
+        <ItemTable filter={filter} filterItems={filterItems} decideButtons={decideButtons}/>
       </div>
     </>
   )
